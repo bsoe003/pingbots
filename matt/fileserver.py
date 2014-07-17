98e1cfd17be669ad0f709f68273fbde977812d74
mzenzie <=>
2014-07-17 07:38:22 -0700

    hi

--- matt/fileserver.py
+++ matt/fileserver.py
@@ -1,13 +1,13 @@
 from twisted.protocol import Protocol, Factory
 
-#message format: "vote|name", "add<name>|size"
+#message format: "vote|name", "add|name|size"
 
 class ReceiveRequest(protocol.Protocol):
     def dataReceived(self,data):
         if data.beginswith("vote"):
-            self.factory.queue.vote(data)
+            self.factory.queue.vote(split(data,"|")[1])
         elif data.beginswith("add"):
-            
+            self.factory.queue.add(split(data,"|")[1],split(data,"|")[2])
     def connectionMade(self):
         
 class ReceiveFactory(protocol.Factory):
@@ -17,3 +17,6 @@ class ReceiveFactory(protocol.Factory):
 
 class SendRequest(protocol.Protocol):
     pass
+
+factory = ReceiveFactory()
+reactor.listenTCP(factory, 1337)
