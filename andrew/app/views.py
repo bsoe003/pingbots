fcdd1b09da5a8aa9df6c66d2a69d169df90eabc4
Andrew Paon <ajpaon@wpi.edu>
2014-07-17 08:10:02 -0700

    added a new categories view

--- andrew/app/views.py
+++ andrew/app/views.py
@@ -1,5 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect
+
+navbar = ["Action", "Nature", "Sports", "Space"]
 
 @app.route('/')
 @app.route('/index')
@@ -8,9 +10,24 @@ def index():
     category = "Most Popular Among Interns"
     queue = ["Hackathon 3D", "Mark Dankberg's big day", "Exede vs. Predator",
              "Sleeping on a Plane", "Test Video 1.5"]
-    navbar = ["Action", "Nature", "Sports", "Space"]
+
     return render_template("base.html",
                            title=title,
                            category=category,
                            queue=queue,
+                           navbar=navbar)
+
+@app.route('/categories/<category>')
+def category(category):
+    cats = {'Popular':["Hackathon 3D",
+                       "Mark Dankberg's Big Day",
+                       "Exede vs. Predator",
+                       "Sleeping on a Plane"],
+            'Sports':["Local Sports Stars Beat Better Team",
+                      "Man Punts Giant Football",
+                      "The Pitcher and the Pilot"]}
+    return render_template("base.html",
+                           title="Category Page",
+                           category=category,
+                           queue=cats[category],
                            navbar=navbar)
