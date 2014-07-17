2c1b853aeb51f4b27fa6121f1862a702c62f148c
mzenzie <=>
2014-07-17 08:00:24 -0700

    morp

--- matt/data.py
+++ matt/data.py
@@ -1,11 +1,12 @@
+from twisted.protocol import Protocol, Factory
+import time
 
 class Download(Object):
-    def __init__(self, name, size, votes, times=[]):
+    def __init__(self, name, size, votes, length =0):
         self.name=name
         self.size=size
         self.votes=votes
-        self.times=times
-
+        self.length=length
 class DownloadData(Object):
     def __init__():
         self.downloads=[]
@@ -15,7 +16,33 @@ class DownloadData(Object):
                 download.votes+=1
                 return
     def add(name, size):
-        self.downloads.insert(0, Download(name, size, 1, times))
+        self.downloads.append(Download(name, size, 1, times))
     
     def get_currently_sending():
         return(downloads[0])
+    
+    def sort_by_time():
+        
+        sort(self.downloads, key=lambda x: x[0])
+    
+
+class ReceiveRequest(protocol.Protocol):
+    def dataReceived(self,data):
+        if data.beginswith("vote"):
+            self.factory.queue.vote(split(data,"|")[1])
+        elif data.beginswith("add"):
+            self.factory.queue.add(split(data,"|")[1],split(data,"|")[2])
+    def connectionMade(self):
+        
+class ReceiveFactory(protocol.Factory):
+    def __init__(datastore):
+        self.queue = datastore
+
+
+class SendRequest(protocol.Protocol):
+    pass
+
+def main():
+    datastore = DownloadData()
+    factory = ReceiveFactory(datastore)
+    reactor.listenTCP(factory, 1337)
