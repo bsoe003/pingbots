890f1b8c85ab5bb166f65154b70a4713fb26a72e
Andrew Paon <ajpaon@wpi.edu>
2014-07-17 22:57:22 -0700

    removed a bunch more things

--- andrew/config.py
+++ andrew/config.py
@@ -1,8 +1,2 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-
-CSRF_ENABLED = True
-SECRET_KEY = 'Dankberg1'
