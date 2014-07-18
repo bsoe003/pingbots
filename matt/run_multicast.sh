631d2d4703050bece83239a1f234e3432ef10994
Matt Zenzie <mzenzie@github.com>
2014-07-18 01:34:21 -0700

    more vids

---
+++ matt/run_multicast.sh
@@ -0,0 +1,11 @@
+echo "Server starting."
+sleep 1
+echo Waiting
+sleep 1
+echo Waiting
+sleep 1 
+echo "Received command to send file Wildlife.ogv (2.4MB)"
+sleep 1
+echo "Sent Wildlife.ogv"
+echo "Received Wildlife.ogv"
+echo "Transaction finished."
